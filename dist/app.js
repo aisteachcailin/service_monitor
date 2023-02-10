d9c6f97ad44fbc57585ac2cabdf5f4b80e7d3f4f
Anastasia <73635691+ganyushinaanastasia@users.noreply.github.com>
2023-02-10 11:40:17 +0300

    adaptive

--- dist/app.js
+++ dist/app.js
@@ -3,14 +3,6 @@ var __webpack_exports__ = {};
 /*!***********************!*\
   !*** ./src/js/app.js ***!
   \***********************/
-//переключение пунтков меню "включить/отключить"
-$(".on").click(function () {
-  $(this).css("display", "none");
-  $(".off").css("display", "inline-block");
-});
-$(".off").click(function () {
-  $(this).css("display", "none");
-  $(".on").css("display", "inline-block");
-});
+
 /******/ })()
 ;
